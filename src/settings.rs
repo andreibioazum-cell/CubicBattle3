b36a509c9da9debdc5d973440b417fab425018d6
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-23 15:43:34 +0400

    Update settings.rs

--- src/settings.rs
+++ src/settings.rs
@@ -1,4 +1,8 @@
-use android_activity::{AndroidApp, input::{InputEvent, MotionAction}};
+use android_activity::{
+    AndroidApp,
+    input::{InputEvent, MotionAction},
+    InputStatus,
+};
 use glow::HasContext;
 
 pub fn render(gl: &glow::Context, app: &AndroidApp) -> bool {
@@ -6,18 +10,21 @@ pub fn render(gl: &glow::Context, app: &AndroidApp) -> bool {
         gl.viewport(0, 0, 1280, 720);
         gl.clear_color(0.3, 0.05, 0.05, 1.0);
         gl.clear(glow::COLOR_BUFFER_BIT);
-
-        if let Ok(mut iter) = app.input_events_iter() {
-            while iter.next(|e| {
-                if let InputEvent::MotionEvent(m) = e {
-                    if m.action() == MotionAction::Down {
-                        return true; // любой тап возвращает назад
-                    }
-                }
-                android_activity::InputStatus::Handled
-            }) {}
-        }
-
-        false
     }
+
+    let mut go_back = false;
+
+    if let Ok(mut iter) = app.input_events_iter() {
+        while iter.next(|event| {
+            if let InputEvent::MotionEvent(motion) = event {
+                if motion.action() == MotionAction::Down {
+                    go_back = true; // любой тап возвращает назад
+                }
+            }
+
+            InputStatus::Handled
+        }) {}
+    }
+
+    go_back
 }
