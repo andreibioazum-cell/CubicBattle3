1547ee5888eb3876df70a8292576cc8be3c759ac
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-23 15:36:10 +0400

    Update settings.rs

--- src/settings.rs
+++ src/settings.rs
@@ -1,11 +1,23 @@
+use android_activity::{AndroidApp, input::{InputEvent, MotionAction}};
 use glow::HasContext;
 
-pub fn render(gl: &glow::Context) -> bool {
+pub fn render(gl: &glow::Context, app: &AndroidApp) -> bool {
     unsafe {
-        gl.clear_color(0.2, 0.05, 0.05, 1.0);
+        gl.viewport(0, 0, 1280, 720);
+        gl.clear_color(0.3, 0.05, 0.05, 1.0);
         gl.clear(glow::COLOR_BUFFER_BIT);
-    }
 
-    // Возвращаем true если нажата кнопка Back
-    false
+        if let Ok(mut iter) = app.input_events_iter() {
+            while iter.next(|e| {
+                if let InputEvent::MotionEvent(m) = e {
+                    if m.action() == MotionAction::Down {
+                        return true; // любой тап возвращает назад
+                    }
+                }
+                android_activity::InputStatus::Handled
+            }) {}
+        }
+
+        false
+    }
 }
