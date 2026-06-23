78fecbafe95ea0bbd23f185ef3df8a967c0da77e
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-23 18:20:05 +0400

    Update settings.rs

--- src/settings.rs
+++ src/settings.rs
@@ -1,10 +1,21 @@
 use android_activity::{AndroidApp, input::{InputEvent, MotionAction}, InputStatus};
-use glow::HasContext;
 
-pub fn render(gl: &glow::Context, app: &AndroidApp, _width: i32, _height: i32) -> bool {
-    unsafe {
-        gl.clear_color(0.3, 0.1, 0.1, 1.0);
-        gl.clear(glow::COLOR_BUFFER_BIT);
+pub fn update_and_draw(
+    pixels: &mut [u8], 
+    app: &AndroidApp, 
+    width: usize, 
+    height: usize, 
+    stride: usize
+) -> bool {
+    // Заливаем красным
+    for y in 0..height {
+        for x in 0..width {
+            let idx = (y * stride + x) * 4;
+            pixels[idx] = 100;
+            pixels[idx + 1] = 20;
+            pixels[idx + 2] = 20;
+            pixels[idx + 3] = 255;
+        }
     }
 
     let mut go_back = false;
@@ -12,7 +23,7 @@ pub fn render(gl: &glow::Context, app: &AndroidApp, _width: i32, _height: i32) -
         while iter.next(|ev| {
             if let InputEvent::MotionEvent(m) = ev {
                 if m.action() == MotionAction::Down {
-                    go_back = true; 
+                    go_back = true;
                 }
             }
             InputStatus::Handled
